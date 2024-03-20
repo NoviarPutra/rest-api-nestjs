d44349de49b79f3a288eb74089709be679359999
NoviarPutra <noviarpb@gmail.com>
2024-03-21 01:37:41 +0700

    AuthGuard

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 import { IS_PUBLIC_KEY } from './auth.decorator';
 
 @Injectable()
@@ -40,8 +41,7 @@ export class AuthGuard implements CanActivate {
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] =
-      request.headers.get('Authorization')?.split(' ') || [];
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
 }
